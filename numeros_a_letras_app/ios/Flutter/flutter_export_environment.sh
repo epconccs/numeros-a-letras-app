f7d41c48ba73709c29b28a9245865c4dfd6cc01a
Andrei Torres <atorres@epcon.com.mx>
2019-11-22 11:45:54 -0600

    Publicación AppStore

--- numeros_a_letras_app/ios/Flutter/flutter_export_environment.sh
+++ numeros_a_letras_app/ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/andreitorres/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/andreitorres/Documents/GitHub/epconccs/numeros-a-letras-app/numeros_a_letras_app"
-export "FLUTTER_TARGET=/Users/andreitorres/Documents/GitHub/epconccs/numeros-a-letras-app/numeros_a_letras_app/lib/main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/andreitorres/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/andreitorres/development/flutter/bin/cache/artifacts/engine/ios-release"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
